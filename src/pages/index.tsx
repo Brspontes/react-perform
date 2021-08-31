ca49b140bb565d4ba52eaf44376537ecb5cdd214
Brspontes <brian.robert16@hotmail.com>
2021-08-31 09:01:20 -0300

    correcao prevend default

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,19 @@ export default function Home() {
   const [results, setResults] = useState([])
 
   async function handleSearch (event: FormEvent) {
-    event.preventDefault
+    event.preventDefault()
     
     if(!search.trim())
       return
 
+    console.log(search)
+    
     const response = await fetch(`http://localhost:3333/products?q=${search}`)
+
     const data = await response.json()
 
     setResults(data)
+    
   }
 
   return (
